9a300e8691a8cf08809d38002eab05a97823f71d
Dalton <Lima>
2019-09-01 21:40:50 +0200

    Lay field and make clickable cells

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,20 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.UI;
 
 public class Cell : MonoBehaviour
 {
-    private bool _bomb;
-    private int _neighbors;
+    private bool _hasBomb;
+    private uint _bombCount = 0;
     private Vector2Int _coordinate;
-    
+    private Image _image;
+
+    private void Awake()
+    {
+        _image = GetComponentInChildren<Image>();
+    }
+
+    public bool HasBomb
+    {
+        get => _hasBomb;
+        set => _hasBomb = value;
+    }
+
+    public uint BombCount
+    {
+        get => _bombCount;
+        private set => _bombCount = value;
+    }
+
     private void Start()
     {
-        
+
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log($"HasBomb {HasBomb}");
+        if (HasBomb)
+        {
+            _image.enabled = true;
+        }
+    }
+
+    public void Create(Vector2Int coordinate, bool hasBomb)
+    {
+        _hasBomb = hasBomb;
+        _coordinate = coordinate;
+    }
+
+    public void AddBombCount()
+    {
+        _bombCount++;
     }
     
-    private void Update()
-    {
-        
-    }
 }
+
---
+++ Assets/Scripts/Field.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class Field : MonoBehaviour
+{
+    [SerializeField] private Vector2Int size;
+    [SerializeField] private uint totalBombs;
+    [SerializeField] private GameObject cellPrefab;
+
+    [SerializeField] private Transform cellHolder;
+    
+    // List/Array to hold cells
+    private Cell[,] _cells;// = new int[9,9];  
+    
+    private void Start()
+    {
+        size = new Vector2Int(9, 9);
+        totalBombs = 10;
+        Create(size, totalBombs);
+    }
+
+    private void Create(Vector2Int fieldSize, uint bombs)
+    {
+        Vector2 offset = new Vector2(fieldSize.x/2f, fieldSize.y/2f);
+        _cells = new Cell[fieldSize.x, fieldSize.y];
+        //TODO: Maybe use some presets to define the size/bombs (easy, medium, hard)
+        for (int i = 0; i < size.x; i++)
+        {
+            for (int j = 0; j < size.y; j++)
+            {
+                GameObject newGameObject = Instantiate(cellPrefab, cellHolder);
+                Cell newCell = newGameObject.GetComponent<Cell>();
+                newGameObject.name = $"Cell {i},{j}";
+                newGameObject.transform.position = new Vector3(i - offset.x, j -offset.y, 0);
+                newCell.Create(size, false);
+                _cells[i, j] = newCell;
+            }
+        }
+
+        // Define random bombs
+        // TODO: we can improve this method by removing the already set places from the next batch
+        for (uint i = 0; i < bombs; i++)
+        {
+            var randomIndex = new Vector2Int(Random.Range(0, size.x), Random.Range(0, size.y));
+
+            if (_cells[randomIndex.x, randomIndex.y].HasBomb == false)
+            {
+                _cells[randomIndex.x, randomIndex.y].HasBomb = true;  
+            }
+            else
+            {
+                i--;
+            }
+        }
+        
+    }
+    
+}
