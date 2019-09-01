ff52b9825803c8f0a44c182a6050fafbec36a955
Dalton <Lima>
2019-09-01 22:54:15 +0200

    Calculating the neighbors numbers of bombs

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,6 +1,5 @@
-﻿using System;
+﻿using TMPro;
 using UnityEngine;
-using UnityEngine.Assertions.Must;
 using UnityEngine.UI;
 
 public class Cell : MonoBehaviour
@@ -9,10 +8,12 @@ public class Cell : MonoBehaviour
     private uint _bombCount = 0;
     private Vector2Int _coordinate;
     private Image _image;
-
+    private TMP_Text _text;
+    
     private void Awake()
     {
         _image = GetComponentInChildren<Image>();
+        _text = GetComponentInChildren<TMP_Text>();
     }
 
     public bool HasBomb
@@ -21,36 +22,49 @@ public class Cell : MonoBehaviour
         set => _hasBomb = value;
     }
 
+    public Vector2Int Coordinate
+    {
+        get => _coordinate;
+        set => _coordinate = value;
+    }
+
     public uint BombCount
     {
         get => _bombCount;
         private set => _bombCount = value;
     }
 
-    private void Start()
-    {
-
-    }
-
     private void OnMouseUp()
     {
-        Debug.Log($"HasBomb {HasBomb}");
+        //Debug.Log($"HasBomb {HasBomb}");
         if (HasBomb)
         {
             _image.enabled = true;
+            return;
+        }
+
+        if (BombCount >= 0)
+        {
+            ShowBombCount();
+            return;
         }
     }
 
     public void Create(Vector2Int coordinate, bool hasBomb)
     {
         _hasBomb = hasBomb;
-        _coordinate = coordinate;
+        Coordinate = coordinate;
     }
 
     public void AddBombCount()
     {
         _bombCount++;
     }
-    
-}
 
+    public void ShowBombCount()
+    {
+        _text.text = BombCount.ToString();
+        _text.enabled = true;
+        _image.enabled = false;
+    }
+}
--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Field : MonoBehaviour
 {
@@ -9,7 +10,8 @@ public class Field : MonoBehaviour
     [SerializeField] private Transform cellHolder;
     
     // List/Array to hold cells
-    private Cell[,] _cells;// = new int[9,9];  
+    private Cell[,] _cells;// = new int[9,9];
+    private List<Cell> _bombCells;
     
     private void Start()
     {
@@ -20,7 +22,7 @@ public class Field : MonoBehaviour
 
     private void Create(Vector2Int fieldSize, uint bombs)
     {
-        Vector2 offset = new Vector2(fieldSize.x/2f, fieldSize.y/2f);
+        Vector2 offset = new Vector2(fieldSize.x / 2f, fieldSize.y / 2f);
         _cells = new Cell[fieldSize.x, fieldSize.y];
         //TODO: Maybe use some presets to define the size/bombs (easy, medium, hard)
         for (int i = 0; i < size.x; i++)
@@ -30,28 +32,51 @@ public class Field : MonoBehaviour
                 GameObject newGameObject = Instantiate(cellPrefab, cellHolder);
                 Cell newCell = newGameObject.GetComponent<Cell>();
                 newGameObject.name = $"Cell {i},{j}";
-                newGameObject.transform.position = new Vector3(i - offset.x, j -offset.y, 0);
-                newCell.Create(size, false);
+                newGameObject.transform.position = new Vector3(i - offset.x, j - offset.y, 0);
+                newCell.Create(new Vector2Int(i, j), false);
                 _cells[i, j] = newCell;
             }
         }
 
         // Define random bombs
         // TODO: we can improve this method by removing the already set places from the next batch
+        _bombCells = new List<Cell>();
         for (uint i = 0; i < bombs; i++)
         {
             var randomIndex = new Vector2Int(Random.Range(0, size.x), Random.Range(0, size.y));
 
             if (_cells[randomIndex.x, randomIndex.y].HasBomb == false)
             {
-                _cells[randomIndex.x, randomIndex.y].HasBomb = true;  
+                _cells[randomIndex.x, randomIndex.y].HasBomb = true;
+                _bombCells.Add(_cells[randomIndex.x, randomIndex.y]);
             }
             else
             {
                 i--;
             }
         }
-        
+
+        // Determine all bomb counts
+        foreach (Cell bombCell in _bombCells)
+        {
+            AddBombCountToNeighbors(bombCell.Coordinate);
+        }
     }
-    
+
+    private void AddBombCountToNeighbors(Vector2Int coordinate)
+    {
+        var neighbors = new List<Cell>();
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                // skipping out-of-bounds
+                if (coordinate.x + i < 0 || coordinate.x + i >= size.x) continue;
+                if (coordinate.y + j < 0 || coordinate.y + j >= size.y) continue;
+                // self bomb
+                if (i == 0 && j == 0) continue;
+                _cells[coordinate.x + i, coordinate.y + j].AddBombCount();
+            }
+        }
+    }
 }
